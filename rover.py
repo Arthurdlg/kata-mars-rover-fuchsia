10b0c40d98bf83f1c7b38296369aa72e9f930b2e
Demonis0 <demonisgames@gmail.com>
2025-01-22 16:58:39 +0100

    wrapping edges

--- rover.py
+++ rover.py
@@ -1,16 +1,22 @@
 ﻿from enum import Enum
 
 class Direction(Enum):
-    N = 0
-    E = 1
-    S = 2
-    W = 3
+    NORTH = 0
+    EAST = 1
+    SOUTH = 2
+    WEST = 3
     
 class Movement(Enum):
-    F = 1
-    B = -1
-    L = 0
-    R = 2
+    FORWARD = 1
+    BACKWARD = -1
+    LEFT = 0
+    RIGHT = 2
+    
+class Map():
+    
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
 class Rover():
     
@@ -18,25 +24,28 @@ class Rover():
         self.x = x
         self.y = y
         self.direction = direction
-        
-    def move_fb(self, movement):
-        if self.direction == Direction.N:
-            self.y += movement.value
-        elif self.direction == Direction.S:
-            self.y -= movement.value
-        elif self.direction == Direction.E:
-            self.x += movement.value
-        elif self.direction == Direction.W:
-            self.x -= movement.value
+
+    def move_fb(self, movement, currentMap):
+        half_width = currentMap.width // 2
+        half_height = currentMap.height // 2
+
+        if self.direction == Direction.NORTH:
+            self.y = (self.y + movement.value + half_height) % currentMap.height - half_height
+        elif self.direction == Direction.SOUTH:
+            self.y = (self.y - movement.value + half_height) % currentMap.height - half_height
+        elif self.direction == Direction.EAST:
+            self.x = (self.x + movement.value + half_width) % currentMap.width - half_width
+        elif self.direction == Direction.WEST:
+            self.x = (self.x - movement.value + half_width) % currentMap.width - half_width
             
     def turn_lr(self, movement):
         self.direction = Direction((self.direction.value + movement.value - 1) % 4)
         
-    def bulk_move(self, movements):
+    def bulk_move(self, movements, currentMap):
         for movement in movements:
             print(movement)
-            if movement in [Movement.F, Movement.B]:
-                self.move_fb(movement)
+            if movement in [Movement.FORWARD, Movement.BACKWARD]:
+                self.move_fb(movement, currentMap)
                 print(self.x, self.y, self.direction)
             else:
                 self.turn_lr(movement)
--- test_rover.py
+++ test_rover.py
@@ -1,13 +1,13 @@
 import pytest
 
-from rover import Rover, Movement, Direction
+from rover import Rover, Movement, Direction, Map
 
 
 @pytest.mark.parametrize("x,y,direction,destination_x,destination_y", 
-                         [(0,0,Direction.N,0,1),
-                          (0,0,Direction.S,0,-1),
-                          (0,0,Direction.E,1,0),
-                          (0,0,Direction.W,-1,0)]
+                         [(0, 0, Direction.NORTH, 0, 1),
+                          (0, 0, Direction.SOUTH, 0, -1),
+                          (0, 0, Direction.EAST, 1, 0),
+                          (0, 0, Direction.WEST, -1, 0)]
                         )
 def test_move_forward(x,
                       y,
@@ -15,16 +15,17 @@ def test_move_forward(x,
                       destination_x,
                       destination_y
                       ):
-    rover = Rover(0, 0, direction)
-    rover.move_fb(Movement.F)
+    currentMap = Map(11, 11)
+    rover = Rover(x, y, direction)
+    rover.move_fb(Movement.FORWARD, currentMap)
     
     assert rover.x == destination_x and rover.y == destination_y and rover.direction == direction
 
 @pytest.mark.parametrize("x,y,direction,destination_x,destination_y",
-                         [(0,0,Direction.N,0,-1),
-                          (0,0,Direction.S,0,1),
-                          (0,0,Direction.E,-1,0),
-                          (0,0,Direction.W,1,0)]
+                         [(0, 0, Direction.NORTH, 0, -1),
+                          (0, 0, Direction.SOUTH, 0, 1),
+                          (0, 0, Direction.EAST, -1, 0),
+                          (0, 0, Direction.WEST, 1, 0)]
                          )
 def test_move_backward(x,
                        y,
@@ -32,38 +33,49 @@ def test_move_backward(x,
                        destination_x,
                        destination_y
                        ):
+    currentMap = Map(11, 11)
     rover = Rover(0,0, direction)
-    rover.move_fb(Movement.B)
+    rover.move_fb(Movement.BACKWARD, currentMap)
     
     assert rover.x == destination_x and rover.y == destination_y and rover.direction == direction
 
 @pytest.mark.parametrize("x,y,init_direction,final_direction",
-                         [(0,0,Direction.N, Direction.W),
-                        (0,0,Direction.W, Direction.S),
-                        (0,0,Direction.S, Direction.E),
-                        (0,0,Direction.E, Direction.N)]
+                         [(0, 0, Direction.NORTH, Direction.WEST),
+                          (0, 0, Direction.WEST, Direction.SOUTH),
+                          (0, 0, Direction.SOUTH, Direction.EAST),
+                          (0, 0, Direction.EAST, Direction.NORTH)]
                          )
 def test_turn_left(x, y, init_direction, final_direction):
     rover = Rover(x, y, init_direction)
-    rover.turn_lr(Movement.L)
+    rover.turn_lr(Movement.LEFT)
 
     assert rover.x == x and rover.y == y and rover.direction == final_direction
     
 @pytest.mark.parametrize("x,y,init_direction,final_direction",
-                            [(0,0,Direction.N, Direction.E),
-                            (0,0,Direction.E, Direction.S),
-                            (0,0,Direction.S, Direction.W),
-                            (0,0,Direction.W, Direction.N)]
+                            [(0, 0, Direction.NORTH, Direction.EAST),
+                             (0, 0, Direction.EAST, Direction.SOUTH),
+                             (0, 0, Direction.SOUTH, Direction.WEST),
+                             (0, 0, Direction.WEST, Direction.NORTH)]
                             )
 def test_turn_right(x, y, init_direction, final_direction):
     rover = Rover(x, y, init_direction)
-    rover.turn_lr(Movement.R)
+    rover.turn_lr(Movement.RIGHT)
 
     assert rover.x == x and rover.y == y and rover.direction == final_direction
     
 def test_bulk_move():
-    movements = [Movement.F, Movement.L, Movement.F, Movement.L, Movement.B, Movement.R]
-    rover = Rover(0, 0, Direction.N)
-    rover.bulk_move(movements)
+    currentMap = Map(11, 11)
+    movements = [Movement.FORWARD, Movement.LEFT, Movement.FORWARD, Movement.LEFT, Movement.BACKWARD, Movement.RIGHT]
+    rover = Rover(0, 0, Direction.NORTH)
+    rover.bulk_move(movements, currentMap)
     
-    assert rover.x == -1 and rover.y == 2 and rover.direction == Direction.W
+    assert rover.x == -1 and rover.y == 2 and rover.direction == Direction.WEST
+    
+def test_wrapping_edges():
+    currentMap = Map(11, 11)
+    rover = Rover(0, 0, Direction.NORTH)
+    
+    for _ in range(8):
+        rover.move_fb(Movement.FORWARD, currentMap)
+    
+    assert rover.x == 0 and rover.y == -3 and rover.direction == Direction.NORTH
