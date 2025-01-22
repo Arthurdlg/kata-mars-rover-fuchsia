fa7a6a56728abf30f26860e525db8632fec4117e
Demonis0 <demonisgames@gmail.com>
2025-01-22 16:23:37 +0100

    bulk move

--- rover.py
+++ rover.py
@@ -1,4 +1,17 @@
-﻿
+﻿from enum import Enum
+
+class Direction(Enum):
+    N = 0
+    E = 1
+    S = 2
+    W = 3
+    
+class Movement(Enum):
+    F = 1
+    B = -1
+    L = 0
+    R = 2
+
 class Rover():
     
     def __init__(self, x, y, direction):
@@ -6,22 +19,25 @@ class Rover():
         self.y = y
         self.direction = direction
         
-    def move_forward(self):
-        if self.direction == 'N':
-            self.y += 1
-        elif self.direction == 'S':
-            self.y -= 1
-        elif self.direction == 'E':
-            self.x += 1 
-        elif self.direction == 'W':
-            self.x -= 1
+    def move_fb(self, movement):
+        if self.direction == Direction.N:
+            self.y += movement.value
+        elif self.direction == Direction.S:
+            self.y -= movement.value
+        elif self.direction == Direction.E:
+            self.x += movement.value
+        elif self.direction == Direction.W:
+            self.x -= movement.value
             
-    def move_backward(self):
-        if self.direction == 'N':
-            self.y -= 1
-        elif self.direction == 'S':
-            self.y += 1
-        elif self.direction == 'E':
-            self.x -= 1
-        elif self.direction == 'W':
-            self.x += 1
+    def turn_lr(self, movement):
+        self.direction = Direction((self.direction.value + movement.value - 1) % 4)
+        
+    def bulk_move(self, movements):
+        for movement in movements:
+            print(movement)
+            if movement in [Movement.F, Movement.B]:
+                self.move_fb(movement)
+                print(self.x, self.y, self.direction)
+            else:
+                self.turn_lr(movement)
+                print(self.x, self.y, self.direction)
--- test_rover.py
+++ test_rover.py
@@ -1,33 +1,69 @@
 import pytest
 
-from rover import Rover
+from rover import Rover, Movement, Direction
 
-@pytest.mark.parametrize("direction,destination_x,destination_y", 
-                         [('N',0,1),
-                          ('S',0,-1),
-                          ('E',1,0),
-                          ('W',-1,0)]
+
+@pytest.mark.parametrize("x,y,direction,destination_x,destination_y", 
+                         [(0,0,Direction.N,0,1),
+                          (0,0,Direction.S,0,-1),
+                          (0,0,Direction.E,1,0),
+                          (0,0,Direction.W,-1,0)]
                         )
-def test_move_forward(direction,
+def test_move_forward(x,
+                      y,
+                      direction,
                       destination_x,
                       destination_y
                       ):
     rover = Rover(0, 0, direction)
-    rover.move_forward()
+    rover.move_fb(Movement.F)
     
     assert rover.x == destination_x and rover.y == destination_y and rover.direction == direction
 
-@pytest.mark.parametrize("direction,destination_x,destination_y",
-                         [('N',0,-1),
-                          ('S',0,1),
-                          ('E',-1,0),
-                          ('W',1,0)]
+@pytest.mark.parametrize("x,y,direction,destination_x,destination_y",
+                         [(0,0,Direction.N,0,-1),
+                          (0,0,Direction.S,0,1),
+                          (0,0,Direction.E,-1,0),
+                          (0,0,Direction.W,1,0)]
                          )
-def test_move_backward(direction,
-                      destination_x,
-                      destination_y
-                      ):
+def test_move_backward(x,
+                       y,
+                       direction,
+                       destination_x,
+                       destination_y
+                       ):
     rover = Rover(0,0, direction)
-    rover.move_backward()
+    rover.move_fb(Movement.B)
     
     assert rover.x == destination_x and rover.y == destination_y and rover.direction == direction
+
+@pytest.mark.parametrize("x,y,init_direction,final_direction",
+                         [(0,0,Direction.N, Direction.W),
+                        (0,0,Direction.W, Direction.S),
+                        (0,0,Direction.S, Direction.E),
+                        (0,0,Direction.E, Direction.N)]
+                         )
+def test_turn_left(x, y, init_direction, final_direction):
+    rover = Rover(x, y, init_direction)
+    rover.turn_lr(Movement.L)
+
+    assert rover.x == x and rover.y == y and rover.direction == final_direction
+    
+@pytest.mark.parametrize("x,y,init_direction,final_direction",
+                            [(0,0,Direction.N, Direction.E),
+                            (0,0,Direction.E, Direction.S),
+                            (0,0,Direction.S, Direction.W),
+                            (0,0,Direction.W, Direction.N)]
+                            )
+def test_turn_right(x, y, init_direction, final_direction):
+    rover = Rover(x, y, init_direction)
+    rover.turn_lr(Movement.R)
+
+    assert rover.x == x and rover.y == y and rover.direction == final_direction
+    
+def test_bulk_move():
+    movements = [Movement.F, Movement.L, Movement.F, Movement.L, Movement.B, Movement.R]
+    rover = Rover(0, 0, Direction.N)
+    rover.bulk_move(movements)
+    
+    assert rover.x == -1 and rover.y == 2 and rover.direction == Direction.W
